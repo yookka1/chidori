97300a45acdad4133606a9ca0d9704d82144d387
Yookka1 <yundoff@gmail.com>
2022-11-26 22:46:54 +0300

    Create network service

---
+++ chidori/Services/NetworkService/Endpoint/EndpointProtocol + Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Endpoint + Extension.swift
+//  chidori
+//
+//  Created by Yookka1 on 26.11.22.
+//
+
+import Foundation
+
+extension Endpoint {
+    var headers: [String: String]? {
+       return nil
+    }
+
+    var parameters: [URLQueryItem]? {
+        return nil
+    }
+}
---
+++ chidori/Services/NetworkService/Endpoint/RequestMethod.swift
@@ -0,0 +1,13 @@
+//
+//  RequestMethod.swift
+//  chidori
+//
+//  Created by Yookka1 on 26.11.22.
+//
+
+enum RequestMethod: String {
+    case GET
+    case PUT
+    case POST
+    case DELETE
+}
